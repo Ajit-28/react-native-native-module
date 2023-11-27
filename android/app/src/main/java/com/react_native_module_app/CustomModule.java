ab003b489768dddb285ef58dd464b69c2e6297d5
Ajit Pal <palajit28@gmail.com>
2023-11-27 15:07:14 +0530

    adding custom native module code

--- android/app/src/main/java/com/react_native_module_app/CustomModule.java
+++ android/app/src/main/java/com/react_native_module_app/CustomModule.java
@@ -1,4 +1,30 @@
 package com.react_native_module_app;
 
-public class CustomeModule {
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class CustomModule extends ReactContextBaseJavaModule {
+
+    private static ReactApplicationContext applicationContext;
+
+    public CustomModule(ReactApplicationContext reactApplicationContext) {
+        super(reactApplicationContext);
+        applicationContext = reactApplicationContext;
+    }
+
+    @ReactMethod
+    public void displaytost(){
+        Toast.makeText(applicationContext, "Welcome to react-native world!!", Toast.LENGTH_SHORT).show();
+    }
+    
+    @NonNull
+    @Override
+    public String getName() {
+        return "CustomModule";
+    }
 }
--- android/app/src/main/java/com/react_native_module_app/CustomModulePackage.java
+++ android/app/src/main/java/com/react_native_module_app/CustomModulePackage.java
@@ -1,2 +1,29 @@
-package com.react_native_module_app;public class CustomModulePackage {
+package com.react_native_module_app;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomModulePackage implements ReactPackage {
+
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactApplicationContext) {
+        List<NativeModule> module = new ArrayList<>();
+        module.add(new CustomModule(reactApplicationContext));
+        return module;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactApplicationContext) {
+        return Collections.emptyList();
+    }
 }
